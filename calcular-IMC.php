276286a6842f97977ac5447b1e6aadeb3c17b1d1
Joao Sironi <joaovsironi@gmail.com>
2022-03-24 22:58:14 -0300

    aplicar logica para calculo

--- calcular-IMC.php
+++ calcular-IMC.php
@@ -5,5 +5,13 @@
         <title>"Atividade 7 - Gerencia de configuração"</title> 
     </head>
     <body>
+        <?php
+         $peso = $_GET['peso'];
+         $altura = $_GET['altura'];
+         
+         $imc = $peso/($altura*$altura);
+         
+         $resultado = number_format($imc);
+        ?>
     </body>
 </html>
