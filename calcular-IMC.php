06e9411db0dff3147f244bc559c6e74e1bb5c7e6
Joao Sironi <joaovsironi@gmail.com>
2022-03-24 22:59:06 -0300

    Dinalizando calculo de IMC

--- calcular-IMC.php
+++ calcular-IMC.php
@@ -12,6 +12,13 @@
          $imc = $peso/($altura*$altura);
          
          $resultado = number_format($imc);
+         
+         if(isset($resultado) && $resultado != '0'){;	
+             echo '<h1>Seu IMC é:</h1>';
+             echo '<h2>'.$resultado.'</h2>';
+         }else{
+             echo '<h1>Por favor, utilize apenas numeros!</h1>';	
+         }
         ?>
     </body>
 </html>
